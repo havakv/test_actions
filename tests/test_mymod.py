fe2194ae8f0997faaa452b05c6edf8a13bd83d08
havakv <haavard.kvamme@gmail..com>
2019-10-17 17:39:08 +0200

    break again

--- tests/test_mymod.py
+++ tests/test_mymod.py
@@ -4,8 +4,7 @@ from src.mymod import make_array
 
 def test_make_array():
     a = make_array()
-    # assert a.dtype.type is np.int64
-    assert True
+    assert a.dtype.type is np.int64
 
 def test_fail():
     "now false"
