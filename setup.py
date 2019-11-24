2b802016692d9897c8964625362d3e97818b982f
havakv <haavard.kvamme@gmail..com>
2019-11-24 18:28:19 +0100

    More

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from setuptools import setup, find_packages
 
 requirements = [
-    # 'feather-format>=0.4.0',
+    'torchtuples>=0.1.2',
+    'feather-format>=0.4.0',
     'h5py>=2.9.0',
     'numba>=0.44',
     'scikit-learn>=0.21.2',
     'requests>=2.22.0',
-    'torchtuples>=0.1.2',
 ]
 
 setup(
